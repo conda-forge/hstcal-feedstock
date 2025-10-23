4727534813c771e091c105332ac70de2eb27e952
Joseph Hunkeler <jhunkeler@gmail.com>
2025-10-23 18:19:42 -0400

    Replace clobbered build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,12 +8,7 @@ if [[ $target_platform == osx-* ]]; then
     export FFLAGS="$FFLAGS -isysroot $CONDA_BUILD_SYSROOT"
 fi
 
-mkdir -p build
-cd build
-cmake .. \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo
+./waf configure --prefix=${PREFIX} --release-with-symbols
+./waf build
+./waf install
 
-make
-
-make install
