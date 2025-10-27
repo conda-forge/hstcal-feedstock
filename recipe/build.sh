fb3a6efc4adf1dc49f758eb2e6f8d7dd9143b392
Joseph Hunkeler <jhunkeler@users.noreply.github.com>
2025-10-27 11:15:11 -0400

    Release 2.6.0 by request (#36)
    * Release 2.6.0 by request
    
    * MNT: Re-rendered with conda-smithy 3.52.3 and conda-forge-pinning 2025.10.23.15.25.2
    
    * Replace clobbered build script
    
    * Do not cross-compile on osx_arm64
    
    * MNT: Re-rendered with conda-smithy 3.52.3 and conda-forge-pinning 2025.10.24.11.58.28
    
    Other tools:
    - conda-build 25.9.0
    - rattler-build 0.48.1
    - rattler-build-conda-compat 1.4.9
    
    * You have to cross-compile
    
    * You have to cross-compile (all the way)
    
    * Disabled sizeof check
    
    * Apply sizeof patch
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,12 +8,8 @@ if [[ $target_platform == osx-* ]]; then
     export FFLAGS="$FFLAGS -isysroot $CONDA_BUILD_SYSROOT"
 fi
 
-mkdir -p build
-cd build
-cmake .. \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo
+patch -p0 < ${RECIPE_DIR}/no_sizeof.patch
+./waf configure --prefix=${PREFIX} --release-with-symbols
+./waf build
+./waf install
 
-make
-
-make install
